29d8c43218b879e613117732280b6f4d97d0c52d
abhinavkumar-15 <pathiabhinavkumar@gmail.com>
2026-07-11 11:01:57 +0530

    Time: 0 ms (100%), Space: 12.7 MB (37.45%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def search(self, nums, target):
+        if target in nums:
+            return True
+        else:
+            return False
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        
