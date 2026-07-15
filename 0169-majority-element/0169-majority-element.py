e6d5798d853120cdfa5701636dbdfe7db771cc61
abhinavkumar-15 <pathiabhinavkumar@gmail.com>
2026-07-15 14:36:45 +0530

    Time: 15 ms (18.99%), Space: 21.2 MB (49.67%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq={}
+        for i in nums:
+            if i not in freq:
+                freq[i]=1
+            else:
+                freq[i]+=1
+        val=max(freq.values())
+        for k,v in freq.items():
+            if v==val:
+                return k
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
