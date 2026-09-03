04a3ab7507b52b36b23f4f9bfd71f4c8c43e3f3c
abhinavkumar-15 <pathiabhinavkumar@gmail.com>
2026-09-03 10:26:06 +0530

    Time: 3 ms (62.09%), Space: 19.3 MB (76.47%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -10,19 +10,15 @@ class Solution:
 
         arr = list(freq.items())
 
-        # Bubble sort
         for i in range(len(arr)):
             for j in range(len(arr) - i - 1):
 
-                # Lower frequency first
                 if arr[j][1] > arr[j + 1][1]:
                     arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-                # Same frequency -> larger value first
                 elif arr[j][1] == arr[j + 1][1] and arr[j][0] < arr[j + 1][0]:
                     arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-        # Build answer
         result = []
 
         for num, count in arr:
